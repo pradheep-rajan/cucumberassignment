e9884e24c5ae3637573c5b0a62cded50b641d918
pradheeprajan <pradheep.pr.rajan@gmail.com>
2021-06-21 23:43:34 +0530

    CUcumber assingment

---
+++ CUCUMBER/src/test/java/testrunner/testrunner.java
@@ -0,0 +1,21 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features = "src/test/java/features"
+		,glue = {"stepdefinition"}
+		
+		)
+
+
+
+ 
+public class testrunner {
+	
+}
